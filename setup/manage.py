76e86e9435fc3b16462e86f0f037e9ca77027ea0
Daniel Vajda <166394135+csw48@users.noreply.github.com>
2024-08-19 15:21:49 +0200

    Update manage.py

--- setup/manage.py
+++ setup/manage.py
@@ -1,10 +1,11 @@
-# manage.py
-
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
+    """Run administrative tasks."""
+    settings_module = 'setup.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'setup.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,6 +15,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
